ac318e0c799ce3817ad2447066679ceba9afd96b
Uladzislau Murashka <sm0k3web@gmail.com>
2025-05-17 15:10:19 +0300

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,5 +27,5 @@ app.use('/api', routes);
 
 const PORT = process.env.PORT || 3001;
 sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
+  app.listen(PORT, '0.0.0.0', () => console.log(`Backend running on port ${PORT}`));
 });
